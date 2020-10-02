14e08fe4cd078d47ac9ebb88f278abb8827f92c6
Soumya <soumyanayan.kar@gmail.com>
2020-10-02 20:12:39 +0530

    Committing changes

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Java!");
+        System.out.println("Hello, Java new!");
     }
 }
